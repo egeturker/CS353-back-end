f09ddaca2286cee8e0a247b4d1101223e145ef51
Ruzgar <zzlawlzz52@gmail.com>
2021-04-23 17:42:47 +0300

    Initial commit

---
+++ src/test/java/cs353/proje/ProjeApplicationTests.java
@@ -0,0 +1,13 @@
+package cs353.proje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
