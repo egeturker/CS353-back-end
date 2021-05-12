caa915163fc6b8d756d8dc9a6e6b1a113f86f025
Ruzgar <zzlawlzz52@gmail.com>
2021-05-12 18:42:57 +0300

    Create AllCourierData.java

---
+++ src/main/java/cs353/proje/usecases/courier/dto/AllCourierData.java
@@ -0,0 +1,12 @@
+package cs353.proje.usecases.courier.dto;
+
+import cs353.proje.usecases.loginregister.dto.Courier;
+
+import java.util.List;
+
+public class AllCourierData {
+
+    Courier courier;
+    List<OperateRegion> operateRegions;
+
+}
