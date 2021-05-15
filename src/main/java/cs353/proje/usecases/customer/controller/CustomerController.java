24961fa918f8efd0beb34b103bf7fa4e7aade54d
Ruzgar <zzlawlzz52@gmail.com>
2021-05-15 20:23:23 +0300

    Update CustomerController.java

--- src/main/java/cs353/proje/usecases/customer/controller/CustomerController.java
+++ src/main/java/cs353/proje/usecases/customer/controller/CustomerController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController("CustomerController")
 @RequestMapping("/customer")
-@CrossOrigin
 @RequiredArgsConstructor
 public class CustomerController {
 
