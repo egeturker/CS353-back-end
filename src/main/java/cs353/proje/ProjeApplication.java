e018bf08e06443b9001e5369b1d14e34597ea70d
Ruzgar <zzlawlzz52@gmail.com>
2021-05-15 20:28:39 +0300

    heroku fix

--- src/main/java/cs353/proje/ProjeApplication.java
+++ src/main/java/cs353/proje/ProjeApplication.java
@@ -3,11 +3,13 @@ package cs353.proje;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "repository")
 public class ProjeApplication {
 
 	public static void main(String[] args) {
